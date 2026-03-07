b6415074f96c11a6876729ac9a50314060e2ed47
samao <samao@130.240.108.47>
2026-03-07 19:55:22 +0100

    två rader på stopevent

--- src/labb6/carsim/StopEvent.java
+++ src/labb6/carsim/StopEvent.java
@@ -1,5 +1,11 @@
 package labb6.carsim;
 
-public class StopEvent {
+//Ärvs så den kan tala om när simulatorn ska stängas
+public class StopEvent extends Event{
+	
+	public StopEvent(double time) {
+		
+		super(time); //skickar tidpunk. till Event så prioq kan sortera på rätt plats
+	}
     
 }
